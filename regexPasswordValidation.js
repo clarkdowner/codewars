125684df829a6d7e09f052317c33171ec52d5066
clarkdowner <cddowner@gmail.com>
2016-12-28 16:54:00 -0800

    regexPasswordValidation starred for return

---
+++ regexPasswordValidation.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/52e1476c8147a7547a000811/train/javascript
+
+// You need to write regex that will validate a password to make sure it meets the follwing criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+
+// Valid passwords will only be alphanumeric characters.
+
+function validate(password) {
+  return /(put answer here)/.test(password);
+}
